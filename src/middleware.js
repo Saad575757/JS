d391da2b9de29c31e080320b68652e51caf1012f
Saad575757 <Smartysaad59@gmail.com>
2025-08-11 23:50:14 +0500

    revert code

--- src/middleware.js
+++ src/middleware.js
@@ -1,17 +1,14 @@
 import { NextResponse } from 'next/server';
-
 export function middleware(request) {
-  // Protect all /dashboard routes
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    const token = request.cookies.get('token')?.value;
-    if (!token) {
-      const loginUrl = new URL('/auth/login', request.url);
-      return NextResponse.redirect(loginUrl);
-    }
+  const response = NextResponse.next();
+  if (request.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
-  return NextResponse.next();
+  return response;
 }
 
+// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/dashboard/:path*'],
+  matcher: '/'
 };
+export { default } from 'next-auth/middleware';
